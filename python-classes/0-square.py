61e03cfc1cdcebd50c19603780281214678f81a5
root <prog58816@gmail.com>
2026-01-09 13:56:54 +0000

    dlsa

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Defines a Square class with a private size attribute."""
+
+
 class Square:
-    def __init__(self,size):
+    """Square class"""
+
+    def __init__(self, size):
         self.__size = size
